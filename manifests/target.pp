ce8d534044d47046497db594cce54df7f2bbd48f
Mark Myers <marcusmyers@gmail.com>
2014-01-02 14:05:20 -0500

    Removed some uneeded stuff

--- manifests/target.pp
+++ manifests/target.pp
@@ -22,33 +22,4 @@ class nagios::target {
     statusmap_image => "base/${::operatingsystem}.gd2",
   }
 
-  @@nagios_service { "check_users_${hostname}":
-     use => "remote-nrpe-users",
-     host_name => "$fqdn",
-  }
-
-  @@nagios_service { "check_load_${hostname}":
-     use => "remote-nrpe-load",
-     host_name => "$fqdn",
-  }
-
-  @@nagios_service { "check_zombie_procs_${hostname}":
-      use => "remote-nrpe-zombie-procs",
-      host_name => "$fqdn",
-  }
-
-  @@nagios_service { "check_total_procs_${hostname}":
-     use => "remote-nrpe-total-procs",
-     host_name => "$fqdn",
-  }
-
-  @@nagios_service { "check_swap_${hostname}":
-     use => "remote-nrpe-swap",
-     host_name => "$fqdn",
-  }
-
-  @@nagios_service { "check_all_disks_${hostname}":
-    use => "remote-nrpe-all-disks",
-    host_name => "$fqdn",
-  }
 }
