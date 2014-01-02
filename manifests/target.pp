0ab4cba6dc93d4344d8d5b6f05056990b9aa3aca
Mark Myers <marcusmyers@gmail.com>
2014-01-02 14:00:31 -0500

    Added all host stuff to target

--- manifests/target.pp
+++ manifests/target.pp
@@ -14,4 +14,41 @@ class nagios::target {
     notification_period => '24x7',
     service_description => "${::hostname}_check_ping",
   }
+
+  @@nagios_hostextinfo { $fqdn:
+    ensure          => present,
+    icon_image_alt  => $operatingsystem,
+    icon_image      => "base/${::operatingsystem}.png",
+    statusmap_image => "base/${::operatingsystem}.gd2",
+  }
+
+  @@nagios_service { "check_users_${hostname}":
+     use => "remote-nrpe-users",
+     host_name => "$fqdn",
+  }
+
+  @@nagios_service { "check_load_${hostname}":
+     use => "remote-nrpe-load",
+     host_name => "$fqdn",
+  }
+
+  @@nagios_service { "check_zombie_procs_${hostname}":
+      use => "remote-nrpe-zombie-procs",
+      host_name => "$fqdn",
+  }
+
+  @@nagios_service { "check_total_procs_${hostname}":
+     use => "remote-nrpe-total-procs",
+     host_name => "$fqdn",
+  }
+
+  @@nagios_service { "check_swap_${hostname}":
+     use => "remote-nrpe-swap",
+     host_name => "$fqdn",
+  }
+
+  @@nagios_service { "check_all_disks_${hostname}":
+    use => "remote-nrpe-all-disks",
+    host_name => "$fqdn",
+  }
 }
