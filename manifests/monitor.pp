5492e7441947d855115f5a2a664345b0565dc975
Mark Myers <marcusmyers@gmail.com>
2014-01-02 11:12:44 -0500

    First commit

---
+++ manifests/monitor.pp
@@ -0,0 +1,18 @@
+class nagios::monitor {
+
+  # Manage the packages
+  package { ['nagios3', 'nagios-plugins' ]: ensure => installed }
+
+  # Manage the Nagios monitoring service
+  service { 'nagios3':
+    ensure    => running,
+    hasstatus => true,
+    enable    => true,
+    alias     => 'nagios',
+    subscribe => [ Package['nagios3'], Package['nagios-plugin'] ],
+  }
+
+  # Collect resources and populate /etc/nagios/nagios_*.cfg
+  Nagios_host     <<||>> { notify => Service['nagios'] }
+  Nagios_services <<||>> { notify => Service['nagios'] }
+}
