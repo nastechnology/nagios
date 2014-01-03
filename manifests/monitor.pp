ff173997958ee1e90e394d123e06333bb92249a7
Mark Myers <marcusmyers@gmail.com>
2014-01-02 20:05:41 -0500

    Added windows template to template.cfg

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -16,4 +16,20 @@ class nagios::monitor {
   Nagios_host    <<||>> { notify => Service['nagios'] }
   Nagios_service <<||>> { notify => Service['nagios'] }
   Nagios_hostextinfo <<||>> { notify => Service['nagios'] }
+
+  @@nagios_host { 'windows-server':
+    ensure                => present,
+    use                   => 'generic-host',
+    target                => '/etc/nagios3/objects/template.cfg',
+    check_period          => '24x7',
+    check_interval        => '5',
+    retry_interval        => '1',
+    max_check_attempts    => '10',
+    check_command         => 'check-host-alive',
+    notification_period   => '24x7',
+    notification_interval => '30',
+    notification_options  => 'd,r',
+    contact_groups        => 'admins',
+    register              => '0',
+  }
 }
